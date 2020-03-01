5698fa55083e8c12803f559931d98c5fde1d5e3a
FelipeGonzalezY <fgonzalezy@gmail.com>
2020-03-01 17:53:35 -0300

    Prueba de componentes y sus parametros Seccion 3: 12. Componentes en ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenidos al Curso de React Native
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Hello( props){
+//   return <p>{props.title}</p>
+// }
+
+// const Hello = (props) => <p>{props.title}</p>
+
+class Hello extends Component {
+  render(){
+    return <p>{this.props.title}</p>;
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Hello title="Bienvenidos al Curso de React Native"/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
